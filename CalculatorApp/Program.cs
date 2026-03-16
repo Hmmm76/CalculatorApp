28fe94d10557d35f9793b22583acea6a4d572df2
Hmmm76 <peterpetrov2007@gmail.com>
2026-03-16 13:09:27 +0200

    added project

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calc = new Calculator();
+        Console.WriteLine($"Сборът е: {calc.Sum(5, 3)}");
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using CalculatorApp;
+
+namespace TestProject1;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {   
+        Calculator calc = new CalculatorApp.Calculator();
+        
+        int result = calc.Sum(2, 3);
+        
+        Assert.Equal(5, result);
+    }
+}
